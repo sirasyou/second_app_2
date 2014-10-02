4c1567ab171797d08d35c8c3d0c2595be2e60d8f
白石 翔太 <s.shota.s@gmail.com>
2014-10-02 23:34:52 +0900

    route,controller,viewにaboutを追記 静的ページaboutを追加するため

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,14 +4,41 @@ describe "StaticPages" do
   
   describe "GET /static_pages" do
     
-    it "works! (now write some real specs)" do
+    #it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
       # get static_pages_index_path
       # response.status.should be(200)
       
-      visit'/static_pages/home'
-      expect(page).to have_content('Sample APP')
+      #visit'/static_pages/home'
+      #expect(page).to have_content('Sample APP')
       
+    #end
+    
+    #以下Homeページ用のtest
+    describe "Home page" do
+      
+      it "should have the content 'Sample App'" do
+        visit '/static_pages/home'
+        expect(page).to have_content('Sample App')
+      end
+    end
+    
+    #以下Helpページ用のtest
+    describe "Help page" do
+      
+      it "should have the content 'Help'" do
+        visit '/static_pages/help'
+        expect(page).to have_content('Help')
+      end
+    end
+    
+    #以下Aboutページ用のtest
+    describe "About page" do
+      
+      it "should have the content 'About Us'" do 
+        visit '/static_pages/about'
+        expect(page).to have_content('About Us')
+      end
     end
     
   end
