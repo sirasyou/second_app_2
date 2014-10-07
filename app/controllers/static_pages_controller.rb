8a897941fe81b21262afcda9c8a2a5b7b040fa9b
白石 翔太 <s.shota.s@gmail.com>
2014-10-08 07:14:50 +0900

    静的ページcontactを追加する コントローラの作成,viewの作成,routesの設定を実施する

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,7 +8,7 @@ class StaticPagesController < ApplicationController
   def about
   end
   
-  #def contact
-  #end
+  def contact
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   
   get 'static_pages/about'
   
-  #get 'static_pages/contact'
+  get 'static_pages/contact'
 
   resources :microposts
 
