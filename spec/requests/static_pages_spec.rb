726e12cbaa4e1c397e89511b0111b5a41bdd456d
白石 翔太 <s.shota.s@gmail.com>
2014-11-24 15:20:26 +0900

    rspecにアプリケーションヘルパーのテストの追加とリンクの確認のテストを追加
    追加テスト実施のため

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 require 'suopport/utilities.rb'
 
-describe "StaticPages" do
+describe "Static pages" do
 
   # let(:base_content){"Sample App"}
-
-  describe "GET /static_pages" do
 
     #it "works! (now write some real specs)" do
       # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
@@ -20,12 +18,21 @@ describe "StaticPages" do
     #以下Homeページ用のtest
     subject { page }
 
+    shared_examples_for"all static pages" do
+      it { should have_content(heading) }
+    end
+
     describe "Home page" do
       before {visit root_path}
+      let(:heading) {'Sample App'}
+      let(:page_title) {''}
 
+      it_should_behave_like "all static pages"
+=begin
       it { should have_content('Sample App') }
       it { should have_title(full_title('')) }
       it { should_not have_title('| Home') }
+=end
     end
 
     #以下Helpページ用のtest
@@ -55,6 +62,12 @@ describe "StaticPages" do
 
     end
 
-  end
+    it "should have the right links on the layout" do
+      visit root_path
+      click_link"About"
+      expect(page).to have_title('About')
+      click_link"Contact"
+      expect(page).to have_title('Contact')
+    end
 
 end
